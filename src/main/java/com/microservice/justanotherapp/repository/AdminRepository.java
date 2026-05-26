82c81a85855960eebd78ec5d8ac94dde2ab39b28
Tejas Nambiar <tejasnambiar32@gmail.com>
2026-05-26 22:33:13 +0530

    base changes

--- src/main/java/com/microservice/justanotherapp/repository/AdminRepository.java
+++ src/main/java/com/microservice/justanotherapp/repository/AdminRepository.java
@@ -5,10 +5,16 @@ import com.microservice.justanotherapp.entity.Admin;
 import java.util.List;
 import java.util.Optional;
 
+import com.microservice.justanotherapp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  * Simple repository abstraction for Admins. Implemented as an in-memory store in AdminRepositoryImpl.
  */
-public interface AdminRepository {
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long>  {
     List<Admin> findAll();
 
     Optional<Admin> findById(Long id);
